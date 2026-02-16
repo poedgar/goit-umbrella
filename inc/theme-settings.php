1cafd0ed8ac0caf974b2c12dff3fc968c884306c
Olena Shatalova <o.shatalova@goit.ua>
2026-02-16 16:42:37 +0200

    added slogan

--- inc/theme-settings.php
+++ inc/theme-settings.php
@@ -1,21 +1,34 @@
 <?php
 function theme_customize_register($wp_customize)
 {
-	$wp_customize->add_section('contact_settings', [
-		'title' => __('Contact Settings', 'goit-umbrella'),
+	$wp_customize->add_section('general_info_settings', [
+		'title' => __('General Information', 'theme-slug'),
 		'priority' => 30,
 	]);
 
+	// Email
 	$wp_customize->add_setting('contact_email', [
 		'default' => 'info@bettered.global',
 		'sanitize_callback' => 'sanitize_email',
 	]);
-
 	$wp_customize->add_control('contact_email_control', [
-		'label' => __('Contact Email', 'goit-umbrella'),
-		'section' => 'contact_settings',
+		'label' => __('Contact Email', 'theme-slug'),
+		'section' => 'general_info_settings',
 		'settings' => 'contact_email',
 		'type' => 'email',
 	]);
+
+	// Slogan
+	$wp_customize->add_setting('footer_slogan', [
+		'default' => 'ВІД ЗНАНЬ ДО ВПЛИВУ',
+		'sanitize_callback' => 'sanitize_text_field',
+	]);
+	$wp_customize->add_control('footer_slogan_control', [
+		'label' => __('Footer Slogan', 'theme-slug'),
+		'section' => 'general_info_settings',
+		'settings' => 'footer_slogan',
+		'type' => 'text',
+	]);
 }
+
 add_action('customize_register', 'theme_customize_register');
