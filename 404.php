08d9faae22db0080e24d042aa53464e319487afc
Macbook Pro 2015 <macbook@Macbooks-MacBook-Pro.local>
2026-02-25 21:47:29 +0200

    add 404 page

--- 404.php
+++ 404.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
     <main>
-        <div class="container flex flex-col justify-center items-center xl:min-[600px] xl:min-[800px]">
+        <div class="container flex flex-col justify-center items-center xl:min-h-[600px] xl:min-h-[800px]">
             <img src="<?php echo get_template_directory_uri(); ?>/src/images/404.png" alt="404" class="mb-20 md:mb-16 w-full">
             
             <h1 class="max-w-[320px] uppercase text-center text-[32px]/[36px] font-medium">Помилка: Сторінку не знайдено</h1>
