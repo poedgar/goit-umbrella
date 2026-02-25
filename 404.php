460756a842553a5163517b7dd131b48fd7ec1130
Macbook Pro 2015 <macbook@Macbooks-MacBook-Pro.local>
2026-02-25 21:25:55 +0200

    add 404 page

--- 404.php
+++ 404.php
@@ -1,5 +1,9 @@
 <?php get_header(); ?>
-
-<?php get_template_part( 'template-parts/loop', '404' ); ?>
-
+    <main>
+        <div class="container items-center">
+            <img src="<?php echo get_template_directory_uri(); ?>/src/images/404.png" alt="404" class="mx-auto mt-20 mb-10 w-full max-w-md">
+            
+            <h1 class="uppercase text-center text-4xl font-bold mb-4">Помилка: Сторінку не знайдено</h1>
+        </div>
+    </main>
 <?php get_footer();
