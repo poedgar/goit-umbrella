8ad0621f7e40d35111df21e0bfbee293e76cd0d1
Macbook Pro 2015 <macbook@Macbooks-MacBook-Pro.local>
2026-02-26 14:17:44 +0200

    add 404 page

--- 404.php
+++ 404.php
@@ -3,6 +3,11 @@
         <div class="container flex flex-col justify-center items-center min-h-[400px] md:min-h-[600px] xl:min-h-[800px]">
             <img src="<?php echo get_template_directory_uri(); ?>/src/images/404.png" alt="404" class="mb-20 md:mb-16 w-full">
             
+            <video class="mb-8 w-full max-w-2xl rounded-lg shadow-lg" controls autoplay muted loop>
+                <source src="<?php echo get_template_directory_uri(); ?>/src/videos/.webm" type="video/webm">
+                Your browser does not support the video tag.
+            </video>
+            
             <h1 class="max-w-[320px] md:max-w-full uppercase text-[#565656] text-center text-[32px]/[36px] font-medium">Помилка: Сторінку не знайдено</h1>
         </div>
     </main>
