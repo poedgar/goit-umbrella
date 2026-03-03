08a1ee2bfd5130eab008d8368f140a546e8a655c
Macbook Pro 2015 <macbook@Macbooks-MacBook-Pro.local>
2026-03-03 22:27:29 +0200

    menu items opening

--- src/js/video.js
+++ src/js/video.js
@@ -1,13 +1,1 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const playButton = document.getElementById('playButton');
-  const iframe = document.getElementById('impactVideo');
 
-  if (!iframe || !playButton) return;
-
-  playButton.addEventListener('click', function () {
-    // Switch to second video with sound
-
-    iframe.src = "https://www.youtube.com/embed/Dn1zS16jhbg";
-    playButton.style.display = 'none';
-  });
-});
--- views/homepage/impact.php
+++ views/homepage/impact.php
@@ -79,3 +79,19 @@ if (!$show_section) return;
 	</div>
 	</div>
 </section>
+
+<script>
+	document.addEventListener('DOMContentLoaded', function () {
+  const playButton = document.getElementById('playButton');
+  const iframe = document.getElementById('impactVideo');
+
+  if (!iframe || !playButton) return;
+
+  playButton.addEventListener('click', function () {
+    // Switch to second video with sound
+
+    iframe.src = "https://www.youtube.com/embed/Dn1zS16jhbg";
+    playButton.style.display = 'none';
+  });
+});
+</script>
