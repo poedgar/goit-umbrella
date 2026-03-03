859909341777556d155c552bf7924c24c0e4fd94
Macbook Pro 2015 <macbook@Macbooks-MacBook-Pro.local>
2026-03-03 21:00:07 +0200

    menu items opening

--- src/js/video.js
+++ src/js/video.js
@@ -1,33 +1,40 @@
 var player;
 
 function onYouTubeIframeAPIReady() {
-  player = new YT.Player('impactVideo', {
-    videoId: 'yO0d6nJPPsE',
-    playerVars: {
-      autoplay: 1,
-      mute: 1,
-      rel: 0,
-      loop: 1,
-      playlist: 'yO0d6nJPPsE',
-      controls: 0,
-      modestbranding: 1
-    }
-  });
+  player = new YT.Player('impactVideo');
+  console.log('Player ready');
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  const playButton = document.getElementById('playButton');
-  const iframe = document.getElementById('impactVideo');
+// document.addEventListener('DOMContentLoaded', function () {
+//   const playButton = document.getElementById('playButton');
+//   const iframe = document.getElementById('impactVideo');
 
-  if (!iframe || !playButton) return;
+//   if (!iframe || !playButton) return;
+
+//   playButton.addEventListener('click', function () {
+//     // Switch to second video with sound
+//     player.loadVideoById('Dn1zS16jhbg');
+//     player.unMute();
+//     player.setLoop(false);
+
+//     // iframe.src = "https://www.youtube.com/embed/Dn1zS16jhbg?si=h_PYub1HsQaqDb0P&autoplay=1&mute=1&rel=0";
+//     playButton.style.display = 'none';
+//   });
+// });
+
+
+ocument.addEventListener('DOMContentLoaded', function () {
+  var playButton = document.getElementById('playButton');
+  if (!playButton) return;
 
   playButton.addEventListener('click', function () {
-    // Switch to second video with sound
+    if (!player || !player.loadVideoById) {
+      console.log('Player not ready');
+      return;
+    }
     player.loadVideoById('Dn1zS16jhbg');
     player.unMute();
     player.setLoop(false);
-
-    // iframe.src = "https://www.youtube.com/embed/Dn1zS16jhbg?si=h_PYub1HsQaqDb0P&autoplay=1&mute=1&rel=0";
     playButton.style.display = 'none';
   });
 });
