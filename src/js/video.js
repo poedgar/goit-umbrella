5538634ae8962395ca64a990f34f30c96fe0645a
Macbook Pro 2015 <macbook@Macbooks-MacBook-Pro.local>
2026-03-03 20:37:31 +0200

    menu items opening

--- src/js/video.js
+++ src/js/video.js
@@ -1,4 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
+  var player;
+
+  function onYouTubeIframeAPIReady() {
+    player = new YT.Player('impactVideo', {
+      videoId: 'yO0d6nJPPsE',
+      playerVars: {
+        autoplay: 1,
+        mute: 1,
+        rel: 0,
+        loop: 1,
+        playlist: 'yO0d6nJPPsE',
+        controls: 0,
+        modestbranding: 1
+      }
+    });
+  }
+
   const playButton = document.getElementById('playButton');
   const iframe = document.getElementById('impactVideo');
 
@@ -6,9 +23,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
   playButton.addEventListener('click', function () {
     // Switch to second video with sound
-    console.log();
+    player.loadVideoById('Dn1zS16jhbg');
+    player.unMute();
+    player.setLoop(false);
 
-    iframe.src = "https://www.youtube.com/embed/Dn1zS16jhbg?si=h_PYub1HsQaqDb0P&autoplay=1&mute=1&rel=0";
+    // iframe.src = "https://www.youtube.com/embed/Dn1zS16jhbg?si=h_PYub1HsQaqDb0P&autoplay=1&mute=1&rel=0";
     playButton.style.display = 'none';
   });
 });
