1a5eefd864189ec98851e266b3fba8b4de561c94
Macbook Pro 2015 <macbook@Macbooks-MacBook-Pro.local>
2026-03-03 11:19:49 +0200

    add main video functionality

--- src/js/video.js
+++ src/js/video.js
@@ -10,6 +10,8 @@ document.addEventListener('DOMContentLoaded', function () {
     source1.src = "<?= esc_url($video_url['url']); ?>";
     source2.src = "<?= esc_url($video_url['url']); ?>";
 
+    console.log(source1.src, "sourc1.src");
+
     video.load();
 
     video.play();
