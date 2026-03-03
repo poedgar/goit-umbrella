9963c8c0aecd7d5bc2273f98f23f934754c3e17a
Macbook Pro 2015 <macbook@Macbooks-MacBook-Pro.local>
2026-03-03 11:16:02 +0200

    add main video functionality

--- src/js/video.js
+++ src/js/video.js
@@ -1,10 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
   const video = document.getElementById('impactVideo');
   const playButton = document.getElementById('playButton');
+  const source1 = document.getElementById("impactVideoSourceMp4");
+  const source2 = document.getElementById("impactVideoSourceWebm");
 
   if (!video || !playButton) return;
 
   playButton.addEventListener('click', function () {
+    source1.src = "<?= esc_url($video_url['url']); ?>";
+    source2.src = "<?= esc_url($video_url['url']); ?>";
+
+    video.load();
+
     video.play();
     playButton.style.display = 'none';
   });
