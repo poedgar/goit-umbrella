8bcab4a922188ae4aabfb683c38ceeceafac5638
Macbook Pro 2015 <macbook@Macbooks-MacBook-Pro.local>
2026-03-03 19:19:51 +0200

    menu items opening

--- views/homepage/impact.php
+++ views/homepage/impact.php
@@ -51,9 +51,6 @@ if (!$show_section) return;
 					allowfullscreen>
 				</iframe> -->
 
-				<div class="video-container">
-					<iframe class="" id="impactVideo" src="https://www.youtube.com/embed/GBYrd07HAfQ?si=E_9SxJgulx93KkU4&autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-				</div>
 
 				<style>
 				  .video-container iframe {
@@ -73,6 +70,10 @@ if (!$show_section) return;
 				    }
 				  }
 				</style>
+
+				<div class="video-container">
+					<iframe class="" id="impactVideo" src="https://www.youtube.com/embed/GBYrd07HAfQ?si=E_9SxJgulx93KkU4&autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+				</div>
 
 				<button
 					id="playButton"
